0779969a763562b928860a1cd3862ad6b371654e
Escudeiro1 <escudeiro.rafa@gmail.com>
2026-09-17 22:47:58 +0200

    revert(shaders): remove "xBR (lv2, Noisy/Experimental)"
    Operator tried it live: didn't work, confirms the prediction that this
    would read as noise/shimmer rather than a stronger effect, not worth
    keeping around. Removes the menu entry and xbr_noisy.frag.

--- modules/game_shaders/shaders.lua
+++ modules/game_shaders/shaders.lua
@@ -18,9 +18,6 @@ local MAP_SHADERS = { {
     name = 'Map - xBR (lv2, Strong)',
     frag = 'shaders/fragment/xbr_strong.frag'
 }, {
-    name = 'Map - xBR (lv2, Noisy/Experimental)',
-    frag = 'shaders/fragment/xbr_noisy.frag'
-}, {
     name = 'Map - xBR (lv3)',
     frag = 'shaders/fragment/xbr_lv3.frag'
 }, {
